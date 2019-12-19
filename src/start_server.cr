bfee5511a34c7573378acdddfc9dd2a9d6c15955
Michael Wagner <mdwranger@gmail.com>
2019-12-18 21:02:22 -0500

    project init

---
+++ src/start_server.cr
@@ -0,0 +1,16 @@
+require "./app"
+
+if Lucky::Env.development?
+  Avram::Migrator::Runner.new.ensure_migrated!
+  Avram::SchemaEnforcer.ensure_correct_column_mappings!
+end
+Habitat.raise_if_missing_settings!
+
+app_server = AppServer.new
+puts "Listening on http://#{app_server.host}:#{app_server.port}"
+
+Signal::INT.trap do
+  app_server.close
+end
+
+app_server.listen
