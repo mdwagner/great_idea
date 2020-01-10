a74b1f5b8b6efa47fde3c8c4dc23f9bd1a27665c
Michael Wagner <mdwranger@gmail.com>
2020-01-09 23:19:07 -0500

    wip login form

--- great-idea-app/src/index.tsx
+++ great-idea-app/src/index.tsx
@@ -2,14 +2,16 @@ import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import { Home } from './screens/Home';
 import { Details } from './screens/Details';
+import { Login } from './screens/Login';
 
 const AppNavigator = createStackNavigator(
   {
     Home,
     Details,
+    Login,
   },
   {
-    initialRouteName: 'Home',
+    initialRouteName: 'Login',
   }
 );
 
